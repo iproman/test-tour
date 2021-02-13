f545d5bbaa3489e9cf0e83bb875ad050b490e0ea
iproman <itechman@yandex.ru>
2021-02-13 19:24:20 +0500

    Add methods for service

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -4,6 +4,16 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class MessageService {
+  messages: string[] = [];
 
-  constructor() { }
+  constructor() {
+  }
+
+  add(message: string): void {
+    this.messages.push(message);
+  }
+
+  clear(): void {
+    this.messages = [];
+  }
 }
