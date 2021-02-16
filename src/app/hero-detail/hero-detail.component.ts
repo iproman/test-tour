5512b636c421ebddb7e5f5639441ea3db07c84d2
iproman <itechman@yandex.ru>
2021-02-17 03:56:36 +0500

    Remove decorator @Input. No need

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -10,7 +10,7 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero: Hero;
+  hero: Hero;
 
   constructor(
     private route: ActivatedRoute,
