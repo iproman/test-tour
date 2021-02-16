d1f79c3ee397a6452f4a720dfeb705767b542d39
iproman <itechman@yandex.ru>
2021-02-17 02:45:11 +0500

    Change to http request

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,21 +3,25 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
+  private heroesUrl = 'api/heroes';
+
   constructor(
-    private messageService: MessageService
+    private messageService: MessageService,
+    private httpClient: HttpClient
   ) {
   }
 
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
     this.log('HeroService: fetched heroes');
-    return heroes;
+
+    return this.httpClient.get<Hero[]>(this.heroesUrl);
   }
 
   getHero(id: number): Observable<Hero> {
