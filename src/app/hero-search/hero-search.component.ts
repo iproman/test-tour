920bfe37223e6fe0710f0f6e01f3bfbe72570bde
iproman <itechman@yandex.ru>
2021-02-18 05:47:47 +0500

    When emitted new value, do smt to service and assign results to observable

--- src/app/hero-search/hero-search.component.ts
+++ src/app/hero-search/hero-search.component.ts
@@ -13,9 +13,26 @@ export class HeroSearchComponent implements OnInit {
   heroes$: Observable<Hero[]>;
   private searchTerms = new Subject<string>();
 
-  constructor() { }
+  constructor(
+    private heroService: HeroService
+  ) {
+  }
 
   ngOnInit(): void {
+    this.heroes$ = this.searchTerms
+      .pipe(
+        // wait 300ms after each keystroke before considering the term
+        debounceTime(300),
+
+        // ignore new term if same as previous term
+        distinctUntilChanged(),
+
+        // switch to new search observable each time the term changes
+
+        switchMap(
+          (term: string) => this.heroService.searchHeroes(term)
+        ),
+      );
   }
 
   search(value: string): void {
