509b9770f0cb483b4fae6ac9fff379957325dbed
iproman <itechman@yandex.ru>
2021-02-18 05:45:36 +0500

    Emit after every search new value to subject

--- src/app/hero-search/hero-search.component.ts
+++ src/app/hero-search/hero-search.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroService } from '../hero.service';
+import { Observable, Subject } from 'rxjs';
+import { Hero } from '../hero';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-hero-search',
@@ -6,10 +10,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-search.component.css']
 })
 export class HeroSearchComponent implements OnInit {
+  heroes$: Observable<Hero[]>;
+  private searchTerms = new Subject<string>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  search(value: string): void {
+    this.searchTerms.next(value);
+  }
 }
